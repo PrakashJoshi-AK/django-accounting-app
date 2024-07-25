1d17623f1bac77468f254f122dadbcdc001a864d
Prakash Joshi <prakash.joshi.ak@outlook.com>
2024-07-25 14:37:20 +0530

    User login implemented

--- app/views.py
+++ app/views.py
@@ -1,7 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
 
 
 # Create your views here.
 
+@login_required()
 def home(request):
     return render(request, "app/home.html")
