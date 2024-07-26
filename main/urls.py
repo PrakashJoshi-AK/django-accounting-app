818f080c6e7d7e2129077569109cef1be3a9452c
prakash <prakash.joshi.ak@outlook.com>
2024-07-26 12:57:04 +0530

    Logout functionality implemented

--- main/urls.py
+++ main/urls.py
@@ -16,10 +16,25 @@ Including another URLconf
 """
 
 from django.contrib import admin
+from django.contrib.auth import views as auth_views
 from django.urls import path, include
+
+
+    # path("accounts/", include("django.contrib.auth.urls")),
+            #     This will include the following URL patterns:
+
+            # accounts/login/ [name='login']
+            # accounts/logout/ [name='logout']
+            # accounts/password_change/ [name='password_change']
+            # accounts/password_change/done/ [name='password_change_done']
+            # accounts/password_reset/ [name='password_reset']
+            # accounts/password_reset/done/ [name='password_reset_done']
+            # accounts/reset/<uidb64>/<token>/ [name='password_reset_confirm']
+            # accounts/reset/done/ [name='password_reset_complete']
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("accounts/", include("django.contrib.auth.urls")),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(template_name = "registration/app_logged_out.html"), name="logout"),
     path("", include('app.urls')),
 ]
